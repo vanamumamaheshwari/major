f8c14e2a168e4e2ac033a6e7a514a4fba7c9ad00
vanamumamaheshwari <62056384+vanamumamaheshwari@users.noreply.github.com>
2020-03-13 18:46:17 +0530

    Add files via upload

---
+++ Shift.java
@@ -0,0 +1,11 @@
+class Shift{
+
+     public static void main(String []args){
+         int i=5;
+         int j= i<<2;
+         System.out.println(j);
+         int k=i>>2;
+         System.out.println(k);
+         
+     }
+}
