716fb649007ec05f21aff53451096d649b5baae8
vanamumamaheshwari <62056384+vanamumamaheshwari@users.noreply.github.com>
2020-03-13 17:44:06 +0530

    Add files via upload

---
+++ Operator.java
@@ -0,0 +1,12 @@
+class Operator{
+
+     public static void main(String []args){
+         int i=4;
+         i+=2;
+        System.out.println(i);
+        i-=2;
+        System.out.println(i);
+        i*=2;
+         System.out.println(i);
+     }
+}
