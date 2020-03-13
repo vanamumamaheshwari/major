09f39cd5e1e284f7ca37440e4996da02b3352ded
vanamumamaheshwari <62056384+vanamumamaheshwari@users.noreply.github.com>
2020-03-13 18:35:12 +0530

    Add files via upload

---
+++ Bitwise.java
@@ -0,0 +1,12 @@
+class Bitwise{
+
+     public static void main(String []args){
+         int i=5;
+         int j=10;
+         int k=i&j; // Bitwise AND
+         System.out.println(k);
+         int l=i|j; //Bitwise OR
+         System.out.println(l);
+         
+     }
+}
