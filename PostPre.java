b9e021a5e23f1dbb4e2e429848a4dc3b4c07183a
vanamumamaheshwari <62056384+vanamumamaheshwari@users.noreply.github.com>
2020-03-13 18:24:17 +0530

    Add files via upload

---
+++ PostPre.java
@@ -0,0 +1,18 @@
+class PostPre{
+
+     public static void main(String []args){
+         int i=5;
+         int j;
+         j=i++;
+         System.out.println(j+" "+i);
+         int k=5;
+         k=k++;
+         System.out.println(k);
+         int r=++k;
+         System.out.println(r);
+         int l=4;
+         l=--l;
+         System.out.println(l);
+         
+     }
+}
