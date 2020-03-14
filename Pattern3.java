e40b947657777d61d191ead98e1ee1737f3d397f
vanamumamaheshwari <62056384+vanamumamaheshwari@users.noreply.github.com>
2020-03-14 23:22:24 +0530

    Add files via upload

---
+++ Pattern3.java
@@ -0,0 +1,20 @@
+class Pattern3{
+public static void main(String a[])
+{
+for(int i=1;i<=4;i++)
+{
+int k=i;
+int l=1;
+for(int j=1;j<=4;j++)
+{
+if(k<=4){
+System.out.print(k);
+k++;}
+else{
+System.out.print(l);
+l++;}
+}
+System.out.println(" ");
+}
+}
+}
