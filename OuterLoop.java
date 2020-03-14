4aeaca1b4e28fcff7873497a9f33ccff26756a7f
vanamumamaheshwari <62056384+vanamumamaheshwari@users.noreply.github.com>
2020-03-14 20:51:10 +0530

    Add files via upload

---
+++ OuterLoop.java
@@ -0,0 +1,16 @@
+class OuterLoop{
+public static void main(String a[])
+{
+uma :
+for(int i=1;i<=4;i++)
+{
+for(int j=1;j<=4;j++)
+{
+if(i==3)
+break uma;
+System.out.print( i );
+}
+System.out.println(" ");
+}
+}
+}
