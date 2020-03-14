225d7cc56d4e8b8838fe314512c98aa2fc4eaf84
vanamumamaheshwari <62056384+vanamumamaheshwari@users.noreply.github.com>
2020-03-14 15:38:10 +0530

    Add files via upload

---
+++ SwitchCase.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+import java.io.*;
+
+public class SwitchCase{
+
+     
+public static void main(String []args){
+                 
+Scanner sc=new Scanner(System.in);
+       
+int i=sc.nextInt();
+         
+switch(i){
+             
+case 0:
+                 
+System.out.print("zero");
+                 
+break;
+                  
+case 1:
+                 
+System.out.print("one");
+                 
+break;
+                  
+case 2:
+                 
+System.out.print("two");
+                 
+break;
+                  
+case 3:
+                 
+System.out.print("three");
+                 
+break;
+                  
+case 4:
+                 
+System.out.print("four");
+                 
+break;
+                 
+default:
+                   
+System.out.print("some number");
+                 
+         
+}
+      
+     
+}
+
+}
