148d56f5ab1255814e248e0cdf801c9cabd029ce
vanamumamaheshwari <62056384+vanamumamaheshwari@users.noreply.github.com>
2020-03-14 13:39:04 +0530

    Add files via upload

---
+++ Terinary.java
@@ -0,0 +1,23 @@
+class Terinary
+{
+	
+public static void main(String[] args) 
+{
+	    
+int a=10,b=16,c;
+	    
+if(a>b)
+		
+System.out.println("a is greater");
+		
+else
+			
+System.out.println("b is greater");
+			
+c=a>b?1:0;
+			
+System.out.println(c);
+	
+}
+
+}
