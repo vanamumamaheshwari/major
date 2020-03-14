1258cb91661a5887d316c853f38ff5211e62c274
vanamumamaheshwari <62056384+vanamumamaheshwari@users.noreply.github.com>
2020-03-14 12:33:53 +0530

    Add files via upload

---
+++ Greater.java
@@ -0,0 +1,23 @@
+class Greater
+{
+	
+public static void main(String[] args) 
+{
+	    
+ int a=10,b=1,c=15;
+	    
+if(a>b&&a>c)
+		
+System.out.println("a is greater");
+		
+else if(b>c)
+		
+System.out.println("b is greater");
+		
+else
+			
+System.out.println("c is greater");
+	
+}
+
+}
