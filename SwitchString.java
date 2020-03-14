7c1bad65d382d0ceebd8e7551668e24308f17566
vanamumamaheshwari <62056384+vanamumamaheshwari@users.noreply.github.com>
2020-03-14 15:44:17 +0530

    Add files via upload

---
+++ SwitchString.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+import java.io.*;
+
+class SwitchString{
+
+     
+public static void main(String []args){
+               
+Scanner sc=new Scanner(System.in);
+       
+String i=sc.nextLine();
+         
+switch(i){
+             
+case "Hi":
+                 
+System.out.print("Hi");
+                 
+break;
+                  
+case "Hello":
+                 
+System.out.print("Hello");
+                 
+break;
+                 
+default:
+                   
+System.out.print("Hey");
+                 
+         
+}
+      
+     
+}
+
+}
