d16df80dcc4718f1aed814b138a4e6835663ef71
vanamumamaheshwari <62056384+vanamumamaheshwari@users.noreply.github.com>
2020-03-14 22:55:57 +0530

    Add files via upload

---
+++ Pattern2.java
@@ -0,0 +1,16 @@
+class Pattern2{
+public static void main(String a[])
+{
+for(int i=1;i<=4;i++)
+{
+for(int j=1;j<=4;j++)
+{
+if(i==1||i==4||j==1||j==4)
+System.out.print("*");
+else
+System.out.print(" ");
+}
+System.out.println(" ");
+}
+}
+}
