358ab57b005ff1ed1bf9a197775bf3e2d96213ba
vanamumamaheshwari <62056384+vanamumamaheshwari@users.noreply.github.com>
2020-03-14 20:50:27 +0530

    Add files via upload

---
+++ InnerLoop.java
@@ -0,0 +1,15 @@
+class InnerLoop{
+public static void main(String a[])
+{
+for(int i=1;i<=4;i++)
+{
+for(int j=1;j<=4;j++)
+{
+if(i==3)
+break;
+System.out.print( i );
+}
+System.out.println(" ");
+}
+}
+}
