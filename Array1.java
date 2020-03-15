320f55592a9371c5d7ecfa51ae1a511dafd00924
vanamumamaheshwari <62056384+vanamumamaheshwari@users.noreply.github.com>
2020-03-15 19:41:58 +0530

    Add files via upload

---
+++ Array1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+class Array1{
+public static void main(String ar[])
+{
+int n;
+Scanner sc=new Scanner(System.in);
+n=sc.nextInt();
+int a[]=new int[n];
+for(int i:a)
+{
+i=sc.nextInt();
+System.out.println(i);
+}
+}
+}
