9e6172470ae67cd63193d9906e0538a459a654c0
vanamumamaheshwari <62056384+vanamumamaheshwari@users.noreply.github.com>
2020-03-15 23:58:21 +0530

    Add files via upload

---
+++ Constructor.java
@@ -0,0 +1,19 @@
+class Constructor{
+public Constructor()
+{
+System.out.println("hello");
+}
+public Constructor(int i)
+{
+System.out.println(i+" "+"hi");
+}
+public Constructor(int j,int k)
+{
+System.out.println(j+" "+k);
+}
+public static void main(String a[])
+{
+Constructor c=new Constructor(6,7);
+}
+}
+
