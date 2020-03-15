0a299016d0e4418976ae0b65227b24ad25a118a3
vanamumamaheshwari <62056384+vanamumamaheshwari@users.noreply.github.com>
2020-03-15 20:00:09 +0530

    Add files via upload

---
+++ Array2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+class Array2{
+public static void main(String ar[])
+{
+Random r=new Random();
+int a[]=new int[20];
+for(int i:a)
+{
+i=r.nextInt(20);
+System.out.println(i);
+}
+}
+}
