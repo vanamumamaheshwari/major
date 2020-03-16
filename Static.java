041f7e4dd8bc5bbbfaafccf008bcf1543816fb83
vanamumamaheshwari <62056384+vanamumamaheshwari@users.noreply.github.com>
2020-03-16 14:12:06 +0530

    Add files via upload

---
+++ Static.java
@@ -0,0 +1,11 @@
+class Static{
+public static void main(String a[])
+{
+Abc.show();
+}
+}
+class Abc{
+public static void show()
+{
+System.out.print("hello");
+}}
---
+++ Static1.java
@@ -0,0 +1,10 @@
+class Static1{
+public static void display()
+{
+int i=10;
+System.out.print(i);
+}
+public static void main(String a[])
+{
+display();
+}}
