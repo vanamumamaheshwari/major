ef9431c9a82cb5f817af6b5021f494a992954f44
vanamumamaheshwari <62056384+vanamumamaheshwari@users.noreply.github.com>
2020-03-17 23:29:32 +0530

    Add files via upload

---
+++ StringSplit.java
@@ -0,0 +1,11 @@
+class StringSplit{
+public static void main(String args[])
+{
+String s="uma,pooja,sangamitra";
+String n[]=s.split(",");
+for(String i:n)
+{
+System.out.println(i);
+}
+}
+}
---
+++ Wrapper.java
@@ -0,0 +1,8 @@
+class Wrapper{
+public static void main(String a[])
+{
+int i=5;
+Integer obj=new Integer(i);
+System.out.print(obj);
+}
+}
