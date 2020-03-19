6fc40dfd99a671af7002dd440fac1c6905abf71d
vanamumamaheshwari <62056384+vanamumamaheshwari@users.noreply.github.com>
2020-03-20 00:41:33 +0530

    Add files via upload

---
+++ MilliSec.java
@@ -0,0 +1,14 @@
+package QRGenerator;
+
+public class MilliSec {
+
+	public static void main(String[] args) {
+		long start=System.currentTimeMillis();
+		System.out.println(start);
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
