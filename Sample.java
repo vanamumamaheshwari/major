85e24eb393f2ff9290a4a6bc1744b80a7732af05
vanamumamaheshwari <62056384+vanamumamaheshwari@users.noreply.github.com>
2020-05-08 16:22:39 +0530

    Add files via upload

---
+++ Sample.java
@@ -0,0 +1,8 @@
+package org.javabrains.in;
+
+public class Sample {
+	public static void show() {
+		System.out.println("hi");
+	}
+
+}
--- Static.java
+++ Static.java
@@ -1,14 +1,18 @@
 package org.javabrains.in;
+import static java.lang.System.out;
+import org.javabrains.in.Sample;
+import static org.javabrains.in.Sample.show;
 
 public class Static {
-	static String s="hello world";
+	//static String s="hello world";
 	/*static {
 		System.out.println("iam in static");
 	}*/
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("iam in main"+s);
+		out.println("iam in main");
+		show();
 
 	}
 	/*static {
