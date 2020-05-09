b69e4b6e43032b606bde61ee24ea42fa4e9a1380
vanamumamaheshwari <62056384+vanamumamaheshwari@users.noreply.github.com>
2020-05-09 15:04:48 +0530

    Add files via upload

---
+++ Poly1.java
@@ -0,0 +1,19 @@
+package org.javabrains.in;
+
+public class Poly1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Method m=new Method();
+		m.show();
+		m.show(1);
+		}
+}
+class Method{
+	public void show() {
+		System.out.println("iam uma");
+	}
+	public void show(int i) {
+		System.out.println("iam maheshwari");
+	}
+}
