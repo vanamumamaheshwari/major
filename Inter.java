090cdd9ca5d7672a1c078376b528875c3f23f36e
vanamumamaheshwari <62056384+vanamumamaheshwari@users.noreply.github.com>
2020-05-10 22:51:14 +0530

    Add files via upload

--- Inter.java
+++ Inter.java
@@ -7,9 +7,7 @@ public class Inter {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Ammu a=()->{
-			System.out.println("show");
-		};
+		Ammu a=()->System.out.println("show");
 		a.show();
 
 	}
