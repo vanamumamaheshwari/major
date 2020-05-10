e5223723140191075b469705eadc0a0b5490e314
vanamumamaheshwari <62056384+vanamumamaheshwari@users.noreply.github.com>
2020-05-10 22:36:46 +0530

    Add files via upload

--- Interface.java
+++ Interface.java
@@ -1,22 +1,31 @@
-interface Ramesh{
-void display();
-}
-class Ravi implements Ramesh{
-public void display()
-{
-System.out.println("hi");
-}
-public void show()
-{
-System.out.println("hello");
-}
-}
-class Interface{
-public static void main(String a[])
-{
-Ramesh r=new Ravi();
-r.display();
-//r.show();
-}
-}
+package org.javabrains.in;
 
+public class Interface {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Abc r=new R();
+r.show();
+//r.display();
+S s=new R();
+s.print();
+	}
+
+}
+interface Abc{
+	public void show();
+}
+interface S{
+	public void print();
+}
+class R implements Abc,S{
+	public void show() {
+		System.out.println("show");
+	}
+	public void display() {
+		System.out.println("display");
+	}
+	public void print() {
+		System.out.println("print");
+	}
+}
