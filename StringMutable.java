383209811040971ed014efdc3e5dba1fa74c6d66
vanamumamaheshwari <62056384+vanamumamaheshwari@users.noreply.github.com>
2020-05-12 23:46:19 +0530

    Add files via upload

--- StringMutable.java
+++ StringMutable.java
@@ -1,8 +1,14 @@
-class StringMutable{
-public static void main(String args[])
-{
-StringBuffer sb=new StringBuffer("uma");
-sb.append(" maheshwari");
-System.out.println(sb);
-}
+package org.javabrains.in;
+
+public class StringMutable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StringBuffer sbf=new StringBuffer("uma");
+		sbf.append("uma");
+sbf.append("maheshwari");
+sbf.replace(0, 3, "vanam");
+System.out.println(sbf);
+	}
+
 }
