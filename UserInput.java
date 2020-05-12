b1d58c70796a8b7ee7fd6eade836a48c1fa1e237
vanamumamaheshwari <62056384+vanamumamaheshwari@users.noreply.github.com>
2020-05-12 14:55:33 +0530

    Add files via upload

---
+++ UserInput.java
@@ -0,0 +1,9 @@
+package org.javabrains.in;
+
+public class UserInput {
+public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		int i=System.in.read();
+		System.out.println(i);
+		}
+}
