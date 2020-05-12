e9881e520b1923b6595b07a3f2e694b3c13abff0
vanamumamaheshwari <62056384+vanamumamaheshwari@users.noreply.github.com>
2020-05-12 15:59:14 +0530

    Add files via upload

--- UserInput.java
+++ UserInput.java
@@ -1,9 +1,15 @@
 package org.javabrains.in;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class UserInput {
 public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
-		int i=System.in.read();
-		System.out.println(i);
+		InputStreamReader is=new InputStreamReader(System.in);
+		BufferedReader br=new BufferedReader(is);
+		int n=Integer.parseInt(br.readLine());
+		System.out.println(n);
 		}
 }
