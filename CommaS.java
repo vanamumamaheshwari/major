e6b82a06767fec504d42d22c13a6f4fcc455b252
vanamumamaheshwari <62056384+vanamumamaheshwari@users.noreply.github.com>
2020-05-12 23:12:51 +0530

    Add files via upload

---
+++ CommaS.java
@@ -0,0 +1,13 @@
+package org.javabrains.in;
+
+public class CommaS{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str="sai,prem,uma,rishitha";
+        String names[]=str.split(",");
+        for(String val:names )
+        System.out.println(val);
+	}
+
+}
