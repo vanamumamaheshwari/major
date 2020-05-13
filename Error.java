5196d92340d9310070c57f3bd289032224316434
vanamumamaheshwari <62056384+vanamumamaheshwari@users.noreply.github.com>
2020-05-13 23:36:54 +0530

    Add files via upload

--- Error.java
+++ Error.java
@@ -1,28 +1,31 @@
 package org.javabrains.in;
 
+import java.util.Scanner;
+
 public class Error {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		int i,j,k=0;
-		int a[]=new int[4];
+		//int a[]=new int[4];
+		//BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		Scanner sc=new Scanner(System.in);
 		i=8;
-		j=2;
 		try {
+			j=sc.nextInt();
 		k=i/j;
-		for(int c=0;c<=4;c++) {
-			a[c]=c+1;
-		}
-		for(int value:a) {
-			System.out.println(value);
-		}
+		System.out.println("bye");
+		
 		}
 		catch(ArithmeticException e) {
 			System.out.println("cannot divide by zero"+e);
 		}
 		catch(ArrayIndexOutOfBoundsException e) {
 			System.out.println("maximun no of values");
+		}
+		catch(Exception e) {
+			System.out.println(e);
 		}
 		System.out.println(k);
 
