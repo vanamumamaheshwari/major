76ade2abbd93cf29d0f832b886f357ac65965e1d
vanamumamaheshwari <62056384+vanamumamaheshwari@users.noreply.github.com>
2020-05-13 22:55:29 +0530

    Add files via upload

--- Error.java
+++ Error.java
@@ -6,13 +6,23 @@ public class Error {
 		// TODO Auto-generated method stub
 		
 		int i,j,k=0;
+		int a[]=new int[4];
 		i=8;
-		j=0;
+		j=2;
 		try {
 		k=i/j;
+		for(int c=0;c<=4;c++) {
+			a[c]=c+1;
 		}
-		catch(Exception e) {
-			System.out.println("cannot divide by zero");
+		for(int value:a) {
+			System.out.println(value);
+		}
+		}
+		catch(ArithmeticException e) {
+			System.out.println("cannot divide by zero"+e);
+		}
+		catch(ArrayIndexOutOfBoundsException e) {
+			System.out.println("maximun no of values");
 		}
 		System.out.println(k);
 
