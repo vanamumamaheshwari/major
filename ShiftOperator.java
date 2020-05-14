21418394d0bb52b87180bf6aa0c4cc2248d514b9
vanamumamaheshwari <62056384+vanamumamaheshwari@users.noreply.github.com>
2020-05-14 21:53:21 +0530

    Add files via upload

---
+++ ShiftOperator.java
@@ -0,0 +1,15 @@
+package org.javabrains.in;
+
+public class ShiftOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=10;
+int b;
+b=a<<2;
+int c=a>>2;
+System.out.println(b);
+System.out.println(c);
+	}
+
+}
