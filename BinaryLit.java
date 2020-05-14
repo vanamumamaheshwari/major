c881c94b1070cc8c2c1cea6e7b2618194ff08b05
vanamumamaheshwari <62056384+vanamumamaheshwari@users.noreply.github.com>
2020-05-14 22:11:08 +0530

    Add files via upload

---
+++ BinaryLit.java
@@ -0,0 +1,12 @@
+package org.javabrains.in;
+
+public class BinaryLit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int b=0b100_00_00;
+		System.out.println(b);
+
+	}
+
+}
