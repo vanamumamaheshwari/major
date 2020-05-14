06e1b7108396bae94b382ebeb9bacf9b22e83398
vanamumamaheshwari <62056384+vanamumamaheshwari@users.noreply.github.com>
2020-05-14 16:28:04 +0530

    Add files via upload

--- SwitchCase.java
+++ SwitchCase.java
@@ -5,21 +5,23 @@ public class SwitchCase {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 int i=0;
+String s=" ";
 switch(i){
-case 0:
-	System.out.println("zero");
+case 0:s="zero";
+	//System.out.println("zero");
 	break;
-case 1:
-	System.out.println("one");
+case 1:s="one";
+//	System.out.println("one");
 	break;
-case 2:
-	System.out.println("two");
+case 2:s="two";
+//	System.out.println("two");
 	break;
-	default:
-		System.out.println("some no");
+	default:s="some no";
+		//System.out.println("some no");
 	
 	
 }
+System.out.println(s);
 	}
 
 }
