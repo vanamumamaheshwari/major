4f4998ba8bc87579c05f6b7e95de258593dfa157
vanamumamaheshwari <62056384+vanamumamaheshwari@users.noreply.github.com>
2020-05-15 14:36:38 +0530

    Add files via upload

--- BinaryLit.java
+++ BinaryLit.java
@@ -6,7 +6,33 @@ public class BinaryLit {
 		// TODO Auto-generated method stub
 		int b=0b100_00_00;
 		System.out.println(b);
-
+		int i=1;
+		i=i++;
+		System.out.println(i);
+		byte c=1;
+		c+=1;
+		System.out.println(c);
+		for(int j=0;j<3;j++) {
+			switch(j) {
+			case 0:
+					break;
+			case 1:
+				System.out.println("zero");
+			case 2:
+				System.out.println("one");
+			case 3:
+				System.out.println("two");
+				}
+			}
+		System.out.println("done");
+		BinaryLit obj=new BinaryLit(5);
 	}
+		//class Siddu{
+			public BinaryLit() {
+				System.out.println("parent");
+			}
+			public BinaryLit(int s) {
+				System.out.println("child");
+			}
 
 }
