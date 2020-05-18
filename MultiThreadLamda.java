492dacf3ef0960fd8d6718cee911953819f5088f
vanamumamaheshwari <62056384+vanamumamaheshwari@users.noreply.github.com>
2020-05-18 20:51:50 +0530

    Add files via upload

--- MultiThreadLamda.java
+++ MultiThreadLamda.java
@@ -2,7 +2,7 @@ package org.javabrains.in;
 
 public class MultiThreadLamda {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		Thread t1=new Thread(()->
 	{
@@ -23,6 +23,11 @@ public class MultiThreadLamda {
 		t1.start();
 		try {Thread.sleep(1000);} catch(Exception e) {}
 		t2.start();
+		t1.join();
+		System.out.println(t2.isAlive());
+		t2.join();
+		System.out.println(t1.isAlive());
+		System.out.println("bye");
 
 	}
 
