e6671717589250247a8a2914c186b21018cd5e5e
vanamumamaheshwari <62056384+vanamumamaheshwari@users.noreply.github.com>
2020-05-19 18:39:31 +0530

    Add files via upload

---
+++ Counter.java
@@ -0,0 +1,36 @@
+package org.javabrains.in;
+
+public class Counter {
+	int count;
+	public synchronized void increment() {
+		count++;
+	}
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		Counter c=new Counter();
+		Thread t1=new Thread(new Runnable() 
+		{
+			public void run() {
+				for(int i=0;i<1000;i++) {
+					c.increment();
+				}
+			}
+		});
+		Thread t2=new Thread(new Runnable() 
+		{
+			public void run() {
+				for(int i=0;i<1000;i++) {
+					c.increment();
+				}
+			}
+		});
+		t1.start();
+		t2.start();
+		t1.join();
+		t2.join();
+		System.out.println("count"+c.count);
+
+	}
+
+}
